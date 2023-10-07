19e657222ef3bcefea5c66c06153c476318bce6f
Andrei <sokhanpageproofs@gmail.com>
2023-10-07 14:08:25 +0300

    test_01

--- src/04/04_2.ts
+++
@@ -1,5 +0,0 @@
-import {CityType} from "../02/02_02";
-
-export function demolishHousesOnTheStreet(city: CityType, street: string) {
-   city.houses = city.houses.filter(h => h.address.street.title !== street)
-}
